782beeeb892e09c1702f74eae87db3718b7e6b42
khalilsediq <khalilullahsadique1334@gmail.com>
2026-02-22 07:59:03 +0500

    Refactor QuizzApp component to include question marks and improve state management

--- src/components/quizz.jsx
+++ src/components/quizz.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect,  useRef, useState } from 'react'
 import shuffleArray from 'shuffle-array';
 
 const QuizzApp = () => {
@@ -8,7 +8,7 @@ const QuizzApp = () => {
     const [error, setError] = useState(false)
     const [currentIndex, setIndex] = useState(0)
     const [result, setResult] = useState(false)
-
+    const [marks, setMarks] = useState(0)
     useEffect(()=>{
         axios.get('https://the-trivia-api.com/v2/questions')
         .then((res)=>{
@@ -27,27 +27,39 @@ const QuizzApp = () => {
 
     //Next Question logic
     const nextQuestion = ()=>{
+            const selecty = input.current.find(item => item && item.checked);
+    console.log(selecty.value);currentIndex
+        
+        console.log(data[currentIndex].correctAnswer);
+          question[currentIndex].correctAnswer === selecty.value ? setMarks(marks + 10) : null
+
+
         setIndex(currentIndex + 1)
     currentIndex < 9 ? setIndex(currentIndex + 1) : setResult(true)
     }
 
   return (
     <>
-    <h2>Quizz App</h2>
+    <h2>Quizz App {marks} </h2>
     {loading && <h1>Loading please wait......</h1>}
     {error && <h1>An unexpected Error Occured, we will try our best to resolve it</h1>}
-
+    {result && <h2>You got {marks} / 100  </h2>}
     {data && !result  && <div key={data[currentIndex].id}>
         <h1>Q{currentIndex + 1} {data[currentIndex].question.text} </h1>
         {shuffleArray([...data[currentIndex].incorrectAnswers, data[currentIndex].correctAnswer].map((item, index)=>{
             return <div key={`option ${index}`}> 
-            <input type="radio" name='question' id={index} value={item} />
+            {/* <input type="radio" name='question' id={index} value={item}
+        //  ref={el => input.current[currentIndex] = el} 
+         />
+            <label htmlFor={index}>{item}</label> */}
+            <input type="radio" name='question' value={item} id={index} //ref={el => input.current[index] = el} 
+            />
             <label htmlFor={index}>{item}</label>
             </div>
-        }))}
+        }))} <br />
+        <button onClick={nextQuestion}>Next</button>
         </div>
         }
-        <button onClick={nextQuestion}>Next</button>
         
 
 
@@ -61,6 +73,7 @@ const QuizzApp = () => {
             {data[index].question.text}
         </div>
     })} */}
+
     </>
   )
 }
